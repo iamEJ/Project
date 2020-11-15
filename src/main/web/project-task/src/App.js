9931fcfbfaab1270ad6468550d0b27ed51c2fa67
iamEJ <jukneviciute185@gmail.com>
2020-11-15 18:24:01 +0200

    rewriting footer class component to functional

--- src/main/web/project-task/src/App.js
+++ src/main/web/project-task/src/App.js
@@ -10,7 +10,7 @@ import ProjectItem from "./components/ProjectItem";
 import TaskList from "./components/TaskList";
 import HomeJumbo from "./components/HomeJumbo";
 import TaskBord from "./components/TaskBord";
-import Footer from "./components/Footer";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
@@ -22,7 +22,7 @@ function App() {
         <Route path="/edit/:id" exact component={Project} />
         <Route exact key="home" path="/" component={ProjectList} />
         <Route exact key="projects" path="/projects" component={ProjectList} />
-        <Route path="/projects/:id" exact component={ProjectItem} />        
+        <Route path="/projects/:id" exact component={ProjectItem} />
         <Route path="/tasks" exact component={TaskList} />
         <Route path="/projects/:id/taskbord" exact component={TaskBord} />
       </Switch>
---
+++ src/main/web/project-task/src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>&copy; Copyright 2020. All Rights Reserved.</p>
+    </div>
+  );
+}
+
+export default Footer;
